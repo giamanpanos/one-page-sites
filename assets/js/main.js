f1e9bd09f5ac07af3d90f755bbe18e5f04f41165
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-05-26 19:04:11 +0300

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+/*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
+
+/*===== Work Popup =====*/
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("work__button")) {
+    togglePortfolioPopUp();
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+function togglePortfolioPopUp() {
+  document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document
+  .querySelector(".portfolio__popup-close")
+  .addEventListener("click", togglePortfolioPopUp);
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp__thumbnail img").src =
+    portfolioItem.querySelector(".work__img").src;
+  document.querySelector(".portfolio__popup-subtitle span").innerHTML =
+    portfolioItem.querySelector(".work__title").innerHTML;
+  document.querySelector(".portfolio__popup-body").innerHTML =
+    portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
