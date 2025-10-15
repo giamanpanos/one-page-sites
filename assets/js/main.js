7e822a0fef86e042bf1319dfdb336e553b35e064
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-10-15 17:58:04 +0300

    update project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,26 @@
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-let mixerPortfolio = mixitup(".game__container", {
+let mixerPortfolio = mixitup(".work__container", {
   selectors: {
-    target: ".game__card",
+    target: ".work__card",
   },
   animation: {
     duration: 300,
   },
 });
 
-/*===== Link Active Game =====*/
-const linkGame = document.querySelectorAll(".game__item");
+/*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll(".work__item");
 
-function activeGame() {
-  linkGame.forEach((l) => l.classList.remove("active-game"));
-  this.classList.add("active-game");
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
 }
 
-linkGame.forEach((l) => l.addEventListener("click", activeGame));
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
-/*===== Game Popup =====*/
+/*===== Work Popup =====*/
 document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("game__button")) {
+  if (e.target.classList.contains("work__button")) {
     togglePortfolioPopUp();
     portfolioItemDetails(e.target.parentElement);
   }
@@ -36,9 +36,9 @@ document
 
 function portfolioItemDetails(portfolioItem) {
   document.querySelector(".pp__thumbnail img").src =
-    portfolioItem.querySelector(".game__img").src;
+    portfolioItem.querySelector(".work__img").src;
   document.querySelector(".portfolio__popup-subtitle span").innerHTML =
-    portfolioItem.querySelector(".game__title").innerHTML;
+    portfolioItem.querySelector(".work__title").innerHTML;
   document.querySelector(".portfolio__popup-body").innerHTML =
     portfolioItem.querySelector(".portfolio__item-details").innerHTML;
 }
